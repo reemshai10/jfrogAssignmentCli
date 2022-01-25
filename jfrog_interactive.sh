af00ab20a875af0f8b29f376562d5e89bf4de89f
reemshai <reemshai10@gmail.com>
2022-01-25 13:43:00 +0200

    create option to create new local repo

--- jfrog_interactive.sh
+++ jfrog_interactive.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-
+#Global
+url=""
+api_key=""
 
 # this function will config to jfrog artifactory , it will config to artifactory server base on server name and username and password , after it will create a token .
 config(){ 
   echo "This is Reem API for Jfrog artifactory"
-  echo "Please enter server name :"
+  echo "Please enter Url ,example:(https://'name'.jfrog.io/artifactory/) :"
   read server
+  url=$server
   echo "Please enter username :"
   read username
   echo "Please enter password :"
@@ -28,7 +31,9 @@ helpDesk()
    echo "5","list","Returns a list of minimal repository details for all repositories of the specified type."
    echo "6","storage","shows the server storage information."
    echo "7","config","reconfig user configuration (if you want to config diffrent server)."
-   echo "8","quit","Quit the Cli"
+   echo "8","create repository","Creates a new repository in Artifactory"
+   echo "9","update repository","Updates an existing repository "
+   echo "10","quit","Quit the Cli"
    echo
 }
 
@@ -38,7 +43,7 @@ helpDesk()
 helpDesk # show the cli menu
 #echo "You need to chhose option 1-8 :"
 PS3='You need to chhose option 1-8 : ' # to select the option 
-select=("ping" "version" "delete" "add" "list" "storage" "config" "quit")
+select=("ping" "version" "delete" "add" "list" "storage" "config" "create" "update" "quit")
 select choose in "${select[@]}"; do # run over and over untill the input is the quit
     case $choose in
         "ping") 
@@ -74,6 +79,23 @@ select choose in "${select[@]}"; do # run over and over untill the input is the 
         "config") # if the user want to config anther server . 
             config 
             ;;
+        "create") # create a new repository in artifactory.
+            echo "Insert the repository key :"
+            read key
+            #echo "Insert the repository class :"
+            #read class
+            #echo "Insert the repository package type :"
+            #read package_type
+            api_key=$(jfrog rt curl /api/security/apiKey 2>/dev/null  | jq -r '.apiKey') 
+            #echo $api_key
+            #jo -p key=$key rclass=$class packageType=$package_type > rep1.json 
+            mv repository-config.json temp.json  
+            jq -r '.key |= "'$key'"' temp.json > repository-config.json
+            rm temp.json
+            jfrog rt curl -X PUT -H "Content-type:application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -d @repository-config.json /api/repositories/$key 
+            ;;
+        "update")
+            ;;    
         "quit")
             echo "User requested exit" # the exit the CLI 
             exit
